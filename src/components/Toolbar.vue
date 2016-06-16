d2e18be9d0e802c5c5e928a841ff1e60d2f76ab7
Yawenina <xiaoywkate@163.com>
2016-06-16 10:17:16 +0800

    add styles

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,13 +1,15 @@
 <template>
-	<i class="glyphicon glyphicon-plus" @click="addNote"></i>
-	<i class="glyphicon glyphicon-star" 
-		@click="toggleFavorite"
-		:class = {starred:activeNote.favorite}></i>
-	<i class="glyphicon glyphicon-remove" @click="deleteNote"></i>
+	<div id="toolbar">
+		<i class="glyphicon glyphicon-plus" @click="addNote"></i>
+		<i class="glyphicon glyphicon-star" 
+			@click="toggleFavorite"
+			:class = {starred:activeNote.favorite}></i>
+		<i class="glyphicon glyphicon-remove" @click="deleteNote"></i>
+	</div>
 </template>
 
 <script>
-	import {addNote,deleteNote,toggleFavorite} from '../vuex/action.js'
+	import {addNote,deleteNote,toggleFavorite} from '../vuex/actions'
 	export default {
 		vuex:{
 			getters:{
@@ -21,3 +23,26 @@
 		}
 	}
 </script>
+
+<style>
+	#toolbar{
+		float: left;
+		width: 80px;
+		background-color: #30414D;
+		color: #767676;
+		padding: 35px 25px 25px;
+	}
+	#toolbar i{
+		font-size: 30px;
+		margin-bottom: 35px;
+		cursor: pointer;
+		opacity: .8;
+		transition: opacity .5s ease
+	}
+	#toolbar i:hover{
+		opacity: 1
+	}
+	.starred{
+		color: #f7ae4f
+	}
+</style>
